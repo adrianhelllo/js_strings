208b11536b5cf685ec295e4e99f6230a552fa6a8
adrianhelllo <hetessyadrian@gmail.com>
2026-01-16 22:36:12 +0100

    lastindexof: returns last occurrence

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ const myStringVariable = "Mathematics"; // Constants cannot be changed
 
 console.log(myStringVariable.length); // The length property, is not a method!
 console.log(myStringVariable.charAt(5)) // OR use standard indexing! (var[])
-console.log(myStringVariable.indexOf("them")) // returns index of first character
+console.log(myStringVariable.indexOf("at")) // returns index of first character
+console.log(myStringVariable.lastIndexOfindexOf("at")) // While indexof returns the first occurrence, lastindexof returns the lastoccurrence.

a32150a3c41ac907ea562789ab50e8382c021357
adrianhelllo <hetessyadrian@gmail.com>
2026-01-16 22:40:31 +0100

    slicing with slice()

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ console.log(myStringVariable.length); // The length property, is not a method!
 console.log(myStringVariable.charAt(5)); // OR use standard indexing! (var[])
 console.log(myStringVariable.indexOf("at")); // returns index of first character
 console.log(myStringVariable.lastIndexOf("at")); // While indexof returns the first occurrence, lastindexof returns the lastoccurrence.
-console.log()
+console.log(myStringVariable.slice(5, 8)); // Like slicing in python

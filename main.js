8601f7508f48f48e983c3f906b12db8effe13189
adrianhelllo <hetessyadrian@gmail.com>
2026-01-16 22:35:04 +0100

    charat: deprecated, use indexing indexof: indexing function, works with more complex cases too

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const myStringVariable = "Mathematics"; // Constants cannot be changed
 
-console.log(myStringVariable.length)
+console.log(myStringVariable.length); // The length property, is not a method!
+console.log(myStringVariable.charAt(5)) // OR use standard indexing! (var[])
+console.log(myStringVariable.indexOf("them")) // returns index of first character
